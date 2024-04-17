e8f01a5f6652ce94524bb9aa2b7e13a340a939e5
KrzysztofSym <krzysztof.symoniuk@gmail.com>
2024-04-17 23:24:00 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const str = "Ryan Williams - Back End Developer";
+const text = document.getElementById("text");
+window.onload = function(){
+    for (let i = 0; i < str.length; i++) {
+        let span = document.createElement('span');
+        span.innerHTML = str[i] 
+        text.appendChild(span);
+        console.log(str[i])
+        span.style.transform = `rotate(${11*i}deg)`;
+    }
+}
